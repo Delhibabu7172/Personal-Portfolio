8b5f9255909c886c5bbf922c796ff95f647cac95
Delhi Babu <rldb143@gmail.com>
2023-11-27 11:34:09 +0530

    one project added

--- src/Components/Projectcards.js
+++ src/Components/Projectcards.js
@@ -2,9 +2,23 @@ import pro1 from "../assets/firstprojectimg.png"
 import pro2 from "../assets/project2.png"
 import pro3 from "../assets/projectthree.png"
 import pro4 from "../assets/project4.png"
+import pro5 from "../assets/project5.png"
+import pro6 from "../assets/"
+
 
 let projectcarddata=[
+    
     {   id:1,
+        imgsrc:pro5,
+        title:"Netflix-Clone",
+        text:"Built a feature-rich Netflix clone using ReactJS, mirroring the original platform's functionality and design.",
+        view:"https://delhibabu-netflix-app.vercel.app/",
+        source:"https://github.com/Delhibabu7172/NETFLIX-app",
+        using2:"TailWindCSS",
+        using1:"React js",
+        using3:"Firebase"
+    },
+    {   id:2,
         imgsrc:pro1,
         title:"Best Food App",
         text:"Enhanced a food app for better user experience, increasing usage by 30% and boosting downloads by 25% through collaborative improvements.",
@@ -14,7 +28,7 @@ let projectcarddata=[
         using1:"React js",
         using3:"TailWindCSS"
     },
-    {   id:2,
+    {   id:3,
         imgsrc:pro2,
         title:"Image Search App",
         text:"An app that enables users to search and display images based on provided keywords.",
@@ -24,7 +38,18 @@ let projectcarddata=[
         using1:"Javascript",
         using3:"CSS"
     },
-    {   id:3,
+    ,
+    {   id:4,
+        imgsrc:pro6,
+        title:"React-Movie-App",
+        text:"Discover and enjoy movies easily with this app—it's your gateway to see all the movies!",
+        view:"https://delhibabu7172.github.io/Movie-App-JS/",
+        source:"https://github.com/Delhibabu7172/Movie-App-JS",
+        using2:"Javascript",
+        using1:"React js",
+        using3:"CSS"
+    },
+    {   id:5,
         imgsrc:pro3,
         title:"Weather-App-JS",
         text:"It's like a digital window to the outdoors. It uses code to fetch and display weather information, like temperature, humidity, and conditions such as rain or sunshine, for a specific location.",
@@ -34,7 +59,7 @@ let projectcarddata=[
         using1:"React js",
         using3:"CSS"
     },
-    {   id:4,
+    {   id:6,
         imgsrc:pro4,
         title:"Movie-App-JS",
         text:"Discover and enjoy movies easily with this app—it's your gateway to see all the movies!",
